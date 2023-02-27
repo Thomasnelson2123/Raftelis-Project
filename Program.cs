8ee308f2ca8e1f380b7906f66f6e2c6637fb5b87
Thomas <thomasnelson2123@gmail.com>
2023-02-26 17:17:15 -0800

    Commented code, stylized HTML, and formatted table values a bit better

--- Program.cs
+++ Program.cs
@@ -1,19 +1,28 @@
 ﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 using Microsoft.VisualBasic.FileIO;
 
 namespace Raftelis_Project;
 class Program
 { 
+    // parses the text file, builds the HTML visualization, and opens it on your default browser
     static void Main(string[] args)
     {
-        string htmlFile = "index2.html";
-        if(!File.Exists(htmlFile)) {
+        string htmlFile = "index.html";
+        // if set to true, will overwrite existing file
+        bool rewriteFile = true;
+        // declare city and state here to make it easier to apply this program to different city information
+        string city = "Mazama"; 
+        string state = "WA";
+        // build HTML file if it doesn't exist already
+        if(!File.Exists(htmlFile) || rewriteFile) {
             string textFile = "Parcels.txt";
             List<List<string>> table = new List<List<string>>();
-            table = ParseFile(textFile, "|");
+            table = ParseFile(textFile, "|", true, city, state);
             BuildHTMLFile(table, htmlFile);
         }
 
+        // open on default browser
         var p = new Process();
         p.StartInfo = new ProcessStartInfo(htmlFile)
         { 
@@ -22,21 +31,45 @@ class Program
         p.Start();
     }
 
-    static List<List<String>> ParseFile(string fileName, string delimiter) {
+    // this method takes in a text file and parses it to a 2D list, representing the table
+    // can specifiy delimiter type, and whether or not to append a google maps column
+    static List<List<String>> ParseFile(string fileName, string delimiter, bool useGoogleMaps, string city="", string state="") {
         if(!File.Exists(fileName)) {throw new Exception("file name not recognized");}
+        bool firstLine = true; 
         TextFieldParser parser = new TextFieldParser(fileName);    
         List<List<string>> table = new List<List<string>>();    
         using (parser) {
             parser.SetDelimiters(delimiter);
+            // read til end of file
             while (!parser.EndOfData) {
-                string[] row = parser.ReadFields();
+                string[]? row = parser.ReadFields();
                 if (row != null)
+                    if (useGoogleMaps) {
+                        string[] mapsAppendedRow = row.Concat(new string[] {" "}).ToArray();
+                        // if reading the first line, append the column header
+                        if (firstLine) {
+                            mapsAppendedRow[mapsAppendedRow.Length - 1] = "GOOGLE MAPS";
+                            firstLine = false;
+                        }
+                        else {
+                            // get the address index
+                            int addressIndex = table[0].IndexOf("ADDRESS");
+                            string address = row[addressIndex];
+                            // format the address to query the google maps API
+                            address += $", {city}, {state}";
+                            string mapsAddress = "https:/www.google.com/maps/search/?api=1&query=" + Uri.EscapeDataString(address);
+                            mapsAppendedRow[mapsAppendedRow.Length - 1] = mapsAddress;
+                        }
+                        row = mapsAppendedRow;
+                    }
+                    // add row to table (note: row should not be null, gives warning nonetheless)
                     table.Add(row.ToList<string>());       
             }
         }
         return table;
     }
 
+    // test method to print the parsed text file
     static void PrintTextFile(List<List<String>> table) {
         int rowIndex = 0;
         foreach (List<string> row in table) {
@@ -49,31 +82,60 @@ class Program
         }
     }
 
+    // method to create the HTML file, given the parsed text file
     static void BuildHTMLFile(List<List<string>> table, string path) {
         string jqueryScript = "<script src=\"https://code.jquery.com/jquery-3.6.3.js\"></script>\n";
         string javascriptFile = "<script type=\"text/javascript\" src=\"main.js\"></script\n>";
         string pageName = "Raftelis Project";
         string htmlString = "";
-        htmlString += $"<!DOCTYPE html>\n<html>\n<head>\n<title>{pageName}</title>\n</head>\n<body>\n";
+        htmlString += $"<!DOCTYPE html>\n<html>\n<head>\n<title>{pageName}</title>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\"></head>\n<body>\n";
+        // build the HTML table
         string tableString = BuildHTMLTable(table);
         htmlString+=tableString;
         htmlString+=jqueryScript;
         htmlString+= javascriptFile;
         htmlString+= "</body>\n</html>\n";
+
+        // write to file
         File.WriteAllText(path, htmlString);
     }
 
+    // this method builds and returns the HTML table for the front end app
     static string BuildHTMLTable(List<List<string>> table) {
         string tableString = $"<table id=\"Parcels\">\n<thead>\n<tr>\n";
-        for (int i = 0; i < table[0].Capacity; i++) {
+        // following columns have hyperlinks
+        int mapsCol = table[0].IndexOf("GOOGLE MAPS");
+        int linkCol = table[0].IndexOf("LINK");
+        // following columns are treated as currency values, are formatted differently
+        int valueCol = table[0].IndexOf("MARKET_VALUE");
+        int saleCol = table[0].IndexOf("SALE_PRICE");
+        // builds table header
+        for (int i = 0; i < table[0].Count; i++) {
             string id = $"{table[0][i]}";
             tableString+=$"<th id=\"{id}\" class=\"clickable\">{table[0][i]}</th>";
         }
         tableString+="\n</tr>\n</thead>\n<tbody>\n";
+        // iterate over every row and append to HTML
         for (int row = 1; row < table.Count; row++) {
             tableString+="<tr>";
             for (int col = 0; col < table[0].Count; col++) {
-                tableString+=$"<td>{table[row][col]}</td>";
+                if (col == linkCol) {
+                    tableString += $"<td><a href=\"{table[row][col]}\">Site Link</a></td>";
+                }
+                else if (col == mapsCol) {
+                    tableString += $"<td><a href=\"{table[row][col]}\">Maps Link</a></td>";
+                }
+                else if (col == valueCol || col == saleCol) {
+                    // format money values nicely
+                    // to be safe, will ensure column values are formatted to be correctly parsed to decimal
+                    string formated = Regex.Replace(table[row][col], @"[^\d.]", "");
+                    decimal value = decimal.Parse(formated);
+                    formated = string.Format("{0:C}", value);
+                    tableString+=$"<td>{formated}</td>";
+                }
+                else {
+                    tableString+=$"<td>{table[row][col]}</td>";
+                } 
             }
             tableString+="</tr>\n";
         }
@@ -81,6 +143,5 @@ class Program
 
         return tableString;
     }
-
 }
 
--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// function used to sort the rows of the table
+// f: either -1 or 1, determines the order the columns will be sorted
+// n: which column to sort on
+// colId: the id of the column being sorted on
 function sortTable(f,n, colId){
 	var rows = $('#Parcels tbody  tr').get();
 	if (colId == "ADDRESS") {
@@ -5,6 +9,7 @@ function sortTable(f,n, colId){
 			var A = getAddressVal(a);
 			var B = getAddressVal(b);
 			var nameCompare = compare(A[0], B[0]);
+			// street names are equal, sort on street number
 			if (nameCompare == 0) {
 				return compare(A[1], B[1]);
 			}
@@ -36,15 +41,7 @@ function sortTable(f,n, colId){
 		});
 	}
 
-	// takes a row (elm) and returns the specified value of column n in that row
-	function getVal(elm){
-		var v = $(elm).children('td').eq(n).text().toUpperCase();
-		if($.isNumeric(v)){
-			v = parseInt(v,10);
-		}
-		return v;
-	}
-
+	// helper function used to sort rows of the table 
 	function compare(A, B) {
 		if(A < B) {
 			return -1*f;
@@ -54,6 +51,17 @@ function sortTable(f,n, colId){
 		}
 		return 0;
 	}
+
+	// takes a row (elm) and returns the specified value of column n in that row
+	function getVal(elm){
+		var v = $(elm).children('td').eq(n).text().toUpperCase();
+		// treat money amounts as numbers
+		v = v.replace(/[$,]/g, '');
+		if($.isNumeric(v)){
+			v = parseInt(v,10);
+		}
+		return v;
+	}	
 
 	// modified version of getVal used to grab the address. Splits it into street name and number
 	function getAddressVal(elm) {
@@ -66,23 +74,32 @@ function sortTable(f,n, colId){
 		return [streetName, streetNumber];
 	}
 
+	// modified version of getVal for the owner column. 
+	// sorts the column by first name
+	// values in column are either in format "lastName, firstName"
+	// or "companyName". Treat companyName as firstName
 	function getOwnerVal(elm) {
 		var owner = $(elm).children('td').eq(n).text().trim().toUpperCase();
 		var splitName = owner.split(',');
+		// if > 1, then looking at a persons name, return the their first name
 		if (splitName.length > 1) {
 			return splitName.slice(1).join(' ').trim();
 		}
+		// company name, just return the string
 		else {
 			return splitName[0];
 		}
 	}
 
+	// modified version of getVal for the date column.
+	// converts date string into a date
 	function getDateVal(elm) {
 		var date = $(elm).children('td').eq(n).text().trim().toUpperCase();
 		var parts = date.split('/');
 		return new Date(parts[2], parts[0] - 1, parts[1]).getTime();
 	}
 
+	// append sorted rows into the HTML file
 	$.each(rows, function(index, row) {
 		$('#Parcels').children('tbody').append(row);
 	});
@@ -90,9 +107,12 @@ function sortTable(f,n, colId){
 $(function() {
     var headers = document.querySelectorAll("#Parcels th");
     var headerMap = {};
+	// map each column id to either -1 or 1. Used to determine whether
+	// to sort by ascending or descending
     headers.forEach(function(header) {
         headerMap[header.id] = 1;
     });
+	// if a column header is clicked, get the column id, then sort by opposite order as before
     $(".clickable").on("click", function(){    
         var colId = $(this).attr("id");
         headerMap[colId] *= -1;
